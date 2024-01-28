03ae08470b57d9d29ec7508d99aca95560891d67
alejandro <dev@alejandro-ao.com>
2024-01-28 18:32:44 +0100

    2

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 # pip install streamlit langchain lanchain-openai
 
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
 
 def get_response(user_input):
     return "I don't know"
@@ -8,6 +9,9 @@ def get_response(user_input):
 # app config
 st.set_page_config(page_title="Chat with websites", page_icon="🤖")
 st.title("Chat with websites")
+chat_history = [
+  AIMessage(content="Hello, I am a bot. How can I help you?"),
+]
 
 # sidebar
 with st.sidebar:
@@ -18,8 +22,8 @@ with st.sidebar:
 user_query = st.chat_input("Type your message here...")
 if user_query is not None and user_query != "":
   response = get_response(user_query)
-  with st.chat_message("Human"):
-      st.write(user_query)
-  with st.chat_message("AI"):
-      st.write(response)
-    
+  chat_history.append(HumanMessage(content=user_query))
+  chat_history.append(AIMessage(content=response))
+  
+with st.sidebar:
+  st.write(chat_history)
