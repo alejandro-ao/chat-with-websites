13a4c68feccb6cf75dcd0261359b0933a17838eb
alejandro <dev@alejandro-ao.com>
2024-01-28 18:17:32 +0100

    1

---
+++ src/app.py
@@ -0,0 +1,25 @@
+# pip install streamlit langchain lanchain-openai
+
+import streamlit as st
+
+def get_response(user_input):
+    return "I don't know"
+
+# app config
+st.set_page_config(page_title="Chat with websites", page_icon="🤖")
+st.title("Chat with websites")
+
+# sidebar
+with st.sidebar:
+    st.header("Settings")
+    website_url = st.text_input("Website URL")
+    
+# user input
+user_query = st.chat_input("Type your message here...")
+if user_query is not None and user_query != "":
+  response = get_response(user_query)
+  with st.chat_message("Human"):
+      st.write(user_query)
+  with st.chat_message("AI"):
+      st.write(response)
+    
