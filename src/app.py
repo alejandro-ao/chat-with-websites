94c2876b8b259a195a4baf1523391ebf4b508b50
alejandro <dev@alejandro-ao.com>
2024-01-28 18:39:34 +0100

    3

--- src/app.py
+++ src/app.py
@@ -9,9 +9,10 @@ def get_response(user_input):
 # app config
 st.set_page_config(page_title="Chat with websites", page_icon="🤖")
 st.title("Chat with websites")
-chat_history = [
-  AIMessage(content="Hello, I am a bot. How can I help you?"),
-]
+if "chat_history" not in st.session_state:
+  st.session_state.chat_history = [
+    AIMessage(content="Hello, I am a bot. How can I help you?"),
+  ]
 
 # sidebar
 with st.sidebar:
@@ -22,8 +23,8 @@ with st.sidebar:
 user_query = st.chat_input("Type your message here...")
 if user_query is not None and user_query != "":
   response = get_response(user_query)
-  chat_history.append(HumanMessage(content=user_query))
-  chat_history.append(AIMessage(content=response))
+  st.session_state.chat_history.append(HumanMessage(content=user_query))
+  st.session_state.chat_history.append(AIMessage(content=response))
   
 with st.sidebar:
-  st.write(chat_history)
+  st.write(st.session_state.chat_history)
